038022a492659445e20b8a2ca383d0ca5f9d3268
samichahir99-cmyk <samichahir99@gmail.com>
2026-04-14 00:59:01 +0100

    Version_Mobile

---
+++ script.js
@@ -0,0 +1,5 @@
+const toogle = document.getElementById("toogle");
+const navbar = document.getElementById("navbar");
+toogle.addEventListener("click", () => {
+            navbar.classList.toggle("apparait");
+        });
